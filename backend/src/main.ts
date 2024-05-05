572718f8477013c54394bdb03e713a2359c94eb1
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2024-05-05 20:21:38 +0300

    Add Swagger, Helmet, Class validator, and Class transformer (#3)
    * install swagger, class transformer, class validator, and helmet
    
    * configure Swagger and helmet
    
    * use validation pipes globally

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swagger = new DocumentBuilder()
+    .setTitle('Fictional Characters Catalogue')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3001);
 }
 
