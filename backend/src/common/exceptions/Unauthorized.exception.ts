23f01d65befa2cbb6bcb142d756e4be2c6268563
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-05-06 02:15:08 +0300

    remove unneeded exception

--- backend/src/common/exceptions/Unauthorized.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class UnauthorizedException extends HttpException {
-  constructor(errorMessage: string) {
-    super(errorMessage, HttpStatus.UNAUTHORIZED);
-  }
-}
