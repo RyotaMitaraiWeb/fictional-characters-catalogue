f2e92a55b707d803e4d9100db812b9b5839476ff
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-05-05 19:53:00 +0300

    configure Swagger and helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+
+  const swagger = new DocumentBuilder()
+    .setTitle('Fictional Characters Catalogue')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3001);
 }
 
