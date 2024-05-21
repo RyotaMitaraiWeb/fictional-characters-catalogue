8c8e40b7aae823307520e8ff62806fe113d46d16
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-05-21 18:11:14 +0300

    register claims middleware globally

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { ClaimsModule } from './common/modules/claims/claims.module';
+import { ClaimsMiddleware } from './middlewares/claims/claims.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, ClaimsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ClaimsMiddleware).forRoutes('*');
+  }
+}
